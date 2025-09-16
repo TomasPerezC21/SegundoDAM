bc858e49a376909638564a67dfcea23160d9fe04
TomásPérezClaseDAM <13788476@alu.murciaeduca.es>
2025-09-16 13:10:42 +0200

    16 de Septiembre

---
+++ Asignaturas/AccesoDatos/O1 Aplicacion Persistente ficheros/src/logica/Producto.java
@@ -0,0 +1,57 @@
+package logica;
+
+public abstract class Producto {
+
+    private int id;
+    private String nombre;
+    private double precio;
+    private int cantidad;
+
+    public Producto(int id, String nombre, double precio, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+
+    protected Producto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public abstract String mostrarDetalles();
+
+    public abstract double calcularPrecio();
+
+
+}
---
+++ Asignaturas/AccesoDatos/O1 Aplicacion Persistente ficheros/src/logica/Ropa.java
@@ -0,0 +1,40 @@
+package logica;
+
+public class Ropa extends Producto {
+
+    final double IVA_ROPA = 0.1;
+    private String talla;
+    private String material;
+
+    public Ropa(int id, String nombre, double precio, int cantidad, String talla, String material) {
+        super(id, nombre, precio, cantidad);
+        this.talla = talla;
+        this.material = material;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    @Override
+    public String mostrarDetalles() {
+        return "";
+    }
+
+    @Override
+    public double calcularPrecio() {
+        return 0;
+    }
+}
