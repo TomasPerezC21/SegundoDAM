2c6c90cddfd7e4caa597dfb3a55fe76092ddb436
TomásPérezClaseDAM <13788476@alu.murciaeduca.es>
2025-09-18 08:58:52 +0200

    add metodos de estado al main

--- Asignaturas/ProgramacionMultimedia/HolaMundoGit/app/src/main/java/com/dam/holamundogit/MainActivity.kt
+++ Asignaturas/ProgramacionMultimedia/HolaMundoGit/app/src/main/java/com/dam/holamundogit/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.dam.holamundogit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG= "StateChange"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Log.i(TAG, "estoy en onCreate")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "estoy en onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "estoy en onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "estoy en onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "estoy en onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "estoy en onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "estoy en onDestroy")
+    }
+
 }
