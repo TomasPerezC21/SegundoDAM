988f3f44bc2af7ae082a14dfeecb0fc3d31ac783
TomásPérezClaseDAM <13788476@alu.murciaeduca.es>
2025-10-10 12:12:19 +0200

    python primera clase

---
+++ Asignaturas/Python/semana1/edad_usuario.py
@@ -0,0 +1,28 @@
+from datetime import date 
+
+anio_actual = date.today().year 
+
+
+edad_usuario = int(input("Introduce tu edad: "))
+
+nacimiento = (anio_actual - edad_usuario)
+
+nombre_usuario = input("Pon tu nombre: ")
+
+mes = int(input("Introduce el número del mes en el que naciste: "))
+
+
+
+meses = {
+    1: "Enero",
+    2: "Febrero",
+    3: "Marzo",
+    4: "Abril",
+    5: "Mayo",
+    6: "Junio",
+}
+
+mesString = meses.get(mes)
+
+
+print(f"Hola {nombre_usuario}. Tienes {edad_usuario} años y naciste en el mes {mesString} año {nacimiento}." )
