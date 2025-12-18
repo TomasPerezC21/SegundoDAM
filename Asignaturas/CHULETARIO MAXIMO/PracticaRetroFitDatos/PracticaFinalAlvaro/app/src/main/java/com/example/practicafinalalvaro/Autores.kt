3d5508bb264dbc86ab400a4fc56b4ea3dd3a76cc
Tomás <13788476@alu.murciaeduca.es>
2025-12-18 11:42:38 +0100

    pmdm

--- Asignaturas/CHULETARIO MAXIMO/PracticaRetroFitDatos/PracticaFinalAlvaro/app/src/main/java/com/example/practicafinalalvaro/Autores.kt
+++
@@ -1,7 +0,0 @@
-package com.example.practicafinalalvaro
-
-data class Autores (
-    var key: String,
-    var birth_date: String,
-    var name: String){
-}
--- Asignaturas/CHULETARIO MAXIMO/VMappWeb Llorent/VMappWeb/app/src/main/java/com/dam/vmappweb/ActividadesFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.dam.vmappweb
-
-import androidx.fragment.app.Fragment
-
-class ActividadesFragment: Fragment() {
-
-}
